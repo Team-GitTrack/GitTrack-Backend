81607a8e4593fb74ecaa096c7f473d28245f199e
jyk1029 <jyk102999@gmail.com>
2024-03-26 14:07:38 +0900

    refactor :: code format

--- src/main/kotlin/juyeong/backend/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/juyeong/backend/global/error/exception/ErrorCode.kt
@@ -4,21 +4,5 @@ enum class ErrorCode(
     val status: Int,
     val message: String,
 ) {
-    INCORRECT_PASSWORD(400, "Incorrect Password"),
-    INCORRECT_USER(400, "Incorrect User"),
-    NOT_ALLOW_SELF_APPLICATION(400, "Not Allow Self Application"),
-    BAD_FILE_EXTENSION(400, "Bad File Extension"),
-    IMPOSSIBLE_APPLICATION(400, "Impossible Application"),
-
-    TOKEN_INVALID(401, "Token Invalid"),
-    TOKEN_EXPIRED(401, "Token Expired"),
-
-    USER_NOT_FOUND(404, "User Not Found"),
-    FEED_NOT_FOUND(404, "Feed Not Found"),
-
-    ALREADY_ACCOUNT_ID(409, "Already Account Id"),
-    ALREADY_NICKNAME(409, "Already Nickname"),
-    ALREADY_APPLY(409, "Already Apply"),
-
     INTERNAL_SERVER_ERROR(500, "Internal Server Error")
 }
--- src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
+++ src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -26,7 +26,7 @@ class SecurityConfig(
 
             .and()
             .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionCreationPolicy(STATELESS)
 
             .and()
             .authorizeHttpRequests()
