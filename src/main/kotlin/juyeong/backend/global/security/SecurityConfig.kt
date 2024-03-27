555e577835e63c862624466c52ccab547ae99c24
jyk1029 <jyk102999@gmail.com>
2024-03-27 19:20:42 +0900

    refactor :: delete Bcrypt

--- src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
+++ src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
@@ -37,7 +37,4 @@ class SecurityConfig(
             .and().apply(FilterConfig(objectMapper))
             .and().build()
     }
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
