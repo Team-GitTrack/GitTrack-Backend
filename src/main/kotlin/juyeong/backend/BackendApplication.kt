5753e642b44a8360d52e0d934effc0ec60fc4933
jyk1029 <jyk102999@gmail.com>
2024-04-12 12:09:07 +0900

    chore :: resolve build bug

--- src/main/kotlin/juyeong/backend/BackendApplication.kt
+++ src/main/kotlin/juyeong/backend/BackendApplication.kt
@@ -1,9 +1,11 @@
 package juyeong.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class BackendApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
+++ src/main/kotlin/juyeong/backend/global/security/SecurityConfig.kt
@@ -4,18 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import juyeong.backend.global.config.FilterConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
-import org.springframework.security.core.AuthenticationException
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.server.ServerAuthenticationEntryPoint
-import org.springframework.stereotype.Component
-import org.springframework.web.server.ServerWebExchange
-import reactor.core.publisher.Mono
 
 @EnableWebSecurity
 @Configuration
